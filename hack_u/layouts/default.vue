e9e55670ec21352712a33ecfbe5300d97de3ebf5
UNLegume <44322273+UNLegume@users.noreply.github.com>
2019-08-19 23:10:20 +0900

    aaa

---
+++ hack_u/layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+<div id="app">
+  <v-app
+   id="inspire"
+  >
+
+
+    <v-app-bar
+      app
+      color="green"
+    >
+      <v-toolbar-title class="white--text">CATch</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-flex text-xs-center>
+            <nuxt/>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="green darken-3"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
---
+++ hack_u/pages/inspire.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center  
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+
+      <v-card
+      color="#FFF"
+      height="616px"
+      width="512px"
+      >
+        <v-card-title class="headline font-color font-weight-bold">
+          Result
+        </v-card-title>
+        <v-spacer/>
+ <v-list-item>
+
+      </v-list-item>
+  
+      <!-- <v-img 
+        src="info"
+        height="512px"
+        width="512px"
+      ></v-img> -->
+
+      <img :src="info" style="height:512px;width:512px;" />
+      <div class="text-center">
+        <v-btn class="mt-12" outlined color="indigo" to="/">return</v-btn>
+      </div>
+      </v-card>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import axios from 'axios'
+Vue.$http = axios;
+
+export default {
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://c0d0f939.ngrok.io/request/white/mix/1/', { responseType : 'arrayBuffer' })
+      .then(response => {
+          this.info = response;
+      });
+  },
+}
+</script>
+
+<style>
+.theme--dark.v-application{
+  background: #FAFAFA;
+}
+
+.font-color{
+  color: #212121;
+}
+
+</style>
+
